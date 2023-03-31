a7dcd27cd07ffb716c99823dee577b12d94cb563
LOBSTER10000 <111476138+LOBSTER10000@users.noreply.github.com>
2023-03-31 17:30:36 +0900

    관리자 페이지 커밋

--- src/main/java/com/sunny/green/controller/AdminController.java
+++ src/main/java/com/sunny/green/controller/AdminController.java
@@ -13,4 +13,28 @@ public class AdminController {
     return "admin/admin_main";
     }
 
+    @GetMapping("/admin/reservation")
+    public String adminRe() {
+        return "/admin/admin_reservation";
+    }
+
+    @GetMapping("/admin/user1")
+    public String adminUs1(){
+        return "/admin/admin_user1";
+    }
+
+    @GetMapping("/admin/user2")
+    public String adminUs2(){
+        return "/admin/admin_user2";
+    }
+
+    @GetMapping("/admin/bbs1")
+    public String bbs1(){
+        return "/admin/admin_bbs1";
+    }
+
+    @GetMapping("/admin/bbs2")
+    public String bbs2(){
+        return "/admin/admin_bbs2";
+    }
 }
