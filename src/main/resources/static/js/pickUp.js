26528eab24c970167517722ce35ee08260ca0f2a
thxb5 <kjhcyz@naver.com>
2023-04-03 20:14:28 +0900

    pickup 2페이지 카테고리 버튼 구현

--- src/main/resources/static/js/pickUp.js
+++ src/main/resources/static/js/pickUp.js
@@ -92,5 +92,29 @@ $.datepicker.setDefaults({
 //     }
 // });
 
-
 // pickup page2 카테고리 버튼 이벤트
+$(document).ready(function(){
+    $(".pickup_recycle_btn").each(function(index) {
+        $(this).attr('menu-index', index);
+    }).click(function(){
+        // 클릭된 <div>의 menu-index 값을 index 변수에 할당한다
+        const index = $(this).attr('menu-index');
+        // 클릭한 <div>에 clicked_menu 클래스 추가
+        $('.pickup_recycle_btn[menu-index=' + index + ']').addClass('clicked_menu');
+        // 그 외 <div>는 clicked_menu 클래스 삭제
+        $('.pickup_recycle_btn[menu-index!=' + index + ']').removeClass('clicked_menu');
+    });
+});
+
+$(document).ready(function(){
+    $(".pickup_recycle_btn2").each(function(index2) {
+        $(this).attr('menu-index2', index2);
+    }).click(function(){
+        // 클릭된 <div>의 menu-index 값을 index 변수에 할당한다
+        const index2 = $(this).attr('menu-index2');
+        // 클릭한 <div>에 clicked_menu 클래스 추가
+        $('.pickup_recycle_btn2[menu-index2=' + index2 + ']').addClass('clicked_menu');
+        // 그 외 <div>는 clicked_menu 클래스 삭제
+        $('.pickup_recycle_btn2[menu-index2!=' + index2 + ']').removeClass('clicked_menu');
+    });
+});
