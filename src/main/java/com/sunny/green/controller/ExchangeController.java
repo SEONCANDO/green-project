a5ed8bdf5c366691e2a50c67e2f774dfb672cc81
LOBSTER10000 <111476138+LOBSTER10000@users.noreply.github.com>
2023-04-13 11:56:42 +0900

    기초 css 완성

--- src/main/java/com/sunny/green/controller/ExchangeController.java
+++ src/main/java/com/sunny/green/controller/ExchangeController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpSession;
+import java.util.Random;
 
 @Controller
 @RequiredArgsConstructor
@@ -28,6 +29,21 @@ public class ExchangeController {
     @GetMapping("/exchange3")
     public String exchange3(HttpSession httpSession, Model mo){
 
+
+
+
+        //        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"; // 예약번호로 사용할 문자열
+
+//        int length = 8; // 예약번호의 길이
+//        Random random = new Random();
+//        StringBuilder reservationId = new StringBuilder();
+//
+//        for (int i = 0; i < length; i++) {
+//            int index = random.nextInt(chars.length());
+//            reservationId.append(chars.charAt(index));
+//        }
+//
+//        mo.addAttribute("reservation", reservationId);
         return "/exchange/exchange3";
     }
 }
