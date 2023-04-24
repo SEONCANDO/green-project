d146fc370570de2beb1e915e8f651d5487623d3b
bananaroti <118143001+Yewonj927@users.noreply.github.com>
2023-04-24 14:03:28 +0900

    .
    .

--- src/main/java/com/sunny/green/controller/CommentController.java
+++ src/main/java/com/sunny/green/controller/CommentController.java
@@ -1,17 +1,5 @@
 package com.sunny.green.controller;
 
-import com.sunny.green.vo.CommentVo;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+public class CommentController{
 
-@Controller
-@RequiredArgsConstructor
-@RequestMapping("comment")
-public class CommentController {
-
-    
 }
