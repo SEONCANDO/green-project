b9f772cbb6a6f6460259947f322d88f7f3afcbc0
thxb5 <kjhcyz@naver.com>
2023-04-25 19:45:03 +0900

    0425

--- src/main/java/com/sunny/green/service/UserService.java
+++ src/main/java/com/sunny/green/service/UserService.java
@@ -1,7 +1,9 @@
 package com.sunny.green.service;
 
 import com.sunny.green.vo.UserVo;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface UserService {
 
     public UserVo kakaoUser(String code);
