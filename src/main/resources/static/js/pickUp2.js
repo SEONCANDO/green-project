d6b5cde42b6ceb4dc99575cd6ac4c69d2d491607
thxb5 <kjhcyz@naver.com>
2023-05-02 20:52:27 +0900

    0502 2번째 페이지 완성

--- src/main/resources/static/js/pickUp2.js
+++ src/main/resources/static/js/pickUp2.js
@@ -22,14 +22,15 @@ $(document).ready(function(){
     });
 });
 
+// 1차 카테고리 버튼
 const firstCategoryButtons = document.querySelectorAll(".btn_recycleCategory_1st");
 
+// 1차 카테고리 버튼 이벤트
 firstCategoryButtons.forEach(button => {
     button.addEventListener('click', () => {
-        const id = button.id;
-        console.log(id); // 클릭된 버튼의 id 값을 콘솔에 출력
-        let options;
-        const secondCategoryButtons = document.querySelectorAll(".btn_recycleCategory_2st");
+        const id = button.id;   // 클릭된 버튼의 id 값을 가져옴
+        let options;    // options를 통해 선택된 1차 카테고리에 상응하는 2차 카테고리를 붙임
+
         if(id ==='img_washing') {
             options = `
                 <input type="button" class="btn_recycleCategory_2st" value="일반세탁기">
@@ -37,6 +38,10 @@ firstCategoryButtons.forEach(button => {
                 <input type="button" class="btn_recycleCategory_2st" value="탈수기">
             `;
             $("#btn_recycleCategory").html(options);
+
+            // 이미지 태그의 src 값을 변경한다
+            $('#img_recycle_preview').attr('src', '/img/pickup/washing-machine.png');
+
         } else if (id === 'img_refrigerator') {
             options = `
                 <input type="button" class="btn_recycleCategory_2st" value="가정용냉장고">
@@ -45,6 +50,10 @@ firstCategoryButtons.forEach(button => {
                 <input type="button" class="btn_recycleCategory_2st" value="업소용냉장고">
             `;
             $("#btn_recycleCategory").html(options);
+
+            // 이미지 태그의 src 값을 변경한다
+            $('#img_recycle_preview').attr('src', '/img/pickup/refrigerator.png');
+
         } else if (id === 'img_television') {
             options = `
                 <input type="button" class="btn_recycleCategory_2st" value="텔레비전(CRT)">
@@ -52,6 +61,10 @@ firstCategoryButtons.forEach(button => {
                 <input type="button" class="btn_recycleCategory_2st" value="프로젝션 TV">
             `;
             $("#btn_recycleCategory").html(options);
+
+            // 이미지 태그의 src 값을 변경한다
+            $('#img_recycle_preview').attr('src', '/img/pickup/television.png');
+
         } else if (id === 'img_airConditioner') {
             options = `
                 <input type="button" class="btn_recycleCategory_2st" value="에어컨실내기">
@@ -59,26 +72,44 @@ firstCategoryButtons.forEach(button => {
                 <input type="button" class="btn_recycleCategory_2st" value="일체형에어컨">
             `;
             $("#btn_recycleCategory").html(options);
+
+            // 이미지 태그의 src 값을 변경한다
+            $('#img_recycle_preview').attr('src', '/img/pickup/air-conditioner.png');
+
         } else if (id === 'img_solar') {
             options = `
                 <input type="button" class="btn_recycleCategory_2st" value="태양광패널">
             `;
             $("#btn_recycleCategory").html(options);
+
+            // 이미지 태그의 src 값을 변경한다
+            $('#img_recycle_preview').attr('src', '/img/pickup/solar-cell.png');
+
         } else if (id === 'img_etc') {
             options = `
                 <input type="button" class="btn_recycleCategory_2st" value="전자레인지">
                 <input type="button" class="btn_recycleCategory_2st" value="오븐">
             `;
             $("#btn_recycleCategory").html(options);
+
+            // 이미지 태그의 src 값을 변경한다
+            $('#img_recycle_preview').attr('src', '/img/pickup/oven.png');
+
         } else if (id === 'img_computer') {
             options = `
                 <input type="button" class="btn_recycleCategory_2st" value="컴퓨터본체">
             `;
             $("#btn_recycleCategory").html(options);
-        }
-        const recycleCategoryButtons = document.querySelectorAll(".btn_recycleCategory_2st");
 
-        recycleCategoryButtons.forEach((button, index2) => {
+            // 이미지 태그의 src 값을 변경한다
+            $('#img_recycle_preview').attr('src', '/img/pickup/computer.png');
+
+        }
+        // 2차 카테고리 버튼
+        const secondCategoryButtons = document.querySelectorAll(".btn_recycleCategory_2st");
+
+        // 2차 카테고리 버튼 이벤트
+        secondCategoryButtons.forEach((button, index2) => {
             button.setAttribute('menu-index2', index2);
             button.addEventListener('click', () => {
                 const index2 = button.getAttribute('menu-index2');
@@ -87,117 +118,83 @@ firstCategoryButtons.forEach(button => {
             });
         });
 
+        secondCategoryButtons.forEach(button => button.addEventListener('click', addItem));
+
         // 추가 버튼 클릭 시 실행될 함수
         function addItem() {
-            // const secondVal = selectedSecondCategoryButton.value;
-            // const categoryNo = document.querySelector('#category
-            secondCategoryButtons.forEach(button => {
-                button.addEventListener('click', () => {
-                    const secondVal = button.value;
-                    const newButtonHtml = `
-                      <tr style="height: 65px;">
-                        <td class="col-3" style="border-right: 2px solid #d5d2d2; font-family: NanumBarunGothic, serif;">
-                          <div class="text_mainCategory_preview">${id}</div>
-                        </td>
-                        <td class="col-3" style="border-right: 2px solid #d5d2d2; font-family: NanumBarunGothic, serif;">
-                          <div class="text_detailCategory_preview" id="category_no">${secondVal}</div>
-                        </td>
-                        <td class="col-3" style="border-right: 2px solid #d5d2d2; text-align: center; font-family: NanumBarunGothic, serif;">
-                          <button type="button" class="minus" onclick="fnCalcCnt('minus');">
-                            <div style="width: 100%; height: 100%; position: absolute; top: -20px;">-</div>
-                          </button>
-                          <input type="text" class="count_category" name="item_cnt" title="수량 입력" value="1" maxlength="1">
-                          <button type="button" class="plus" onclick="fnCalcCnt('plus');">
-                            <div style="width: 100%; height: 100%; position: absolute; top: -20px;">+</div>
-                          </button>
-                        </td>
-                        <td class="col-3">
-                          <div class="button_inputCategory_delete">
-                            <input type="button" value="X" style="color: red; border: none; font-size: 28px;">
-                          </div>
-                        </td>
-                      </tr>
-                    `;
-                    const preview_row = document.querySelector('#preview_row');
-                    const newTr = document.createElement('tr');
-                    newTr.innerHTML = newButtonHtml;
-                    preview_row.parentNode.insertBefore(newTr, preview_row.nextSibling);
-                });
+            const categoryVal = event.target.value;
+            const previewCategories = document.querySelectorAll('.text_detailCategory_preview');
+            let categoryExists = false;
+
+            // 이미 존재하는 값을 확인
+            previewCategories.forEach(function(previewCategory) {
+                if (previewCategory.innerText === categoryVal) {
+                    categoryExists = true;
+                }
             });
 
-        // secondCategoryButtons.forEach(button => {
-        //     button.addEventListener('click', (buttonValue) => {
-        //         const secondVal = button.val();
-        //         // 추가할 버튼의 HTML
-        //         const newButtonHtml = `
-        //         <tr style="height: 65px;">
-        //             <td class="col-3" style="border-right: 2px solid #d5d2d2; font-family: NanumBarunGothic, serif;">
-        //               <div class="text_mainCategory_preview">${secondVal}</div>
-        //             </td>
-        //             <td class="col-3" style="border-right: 2px solid #d5d2d2; font-family: NanumBarunGothic, serif;">
-        //               <div class="text_detailCategory_preview" id="category_no">${buttonValue}</div>
-        //             </td>
-        //             <td class="col-3" style="border-right: 2px solid #d5d2d2; text-align: center; font-family: NanumBarunGothic, serif;">
-        //               <button type="button" class="minus" onclick="fnCalcCnt('minus');">
-        //                 <div style="width: 100%; height: 100%; position: absolute; top: -20px;">-</div>
-        //               </button>
-        //               <input type="text" class="count_category" name="item_cnt" title="수량 입력" value="1" maxlength="1">
-        //               <button type="button" class="plus" onclick="fnCalcCnt('plus');">
-        //                 <div style="width: 100%; height: 100%; position: absolute; top: -20px;">+</div>
-        //               </button>
-        //             </td>
-        //             <td class="col-3">
-        //               <div class="button_inputCategory_delete">
-        //                 <input type="button" value="X" style="color: red; border: none; font-size: 28px;">
-        //               </div>
-        //             </td>
-        //         </tr>
-        //       `;
-        //
-        //         // 추가할 위치의 tr 태그 선택
-        //         const preview_row = document.querySelector('#preview_row');
-        //
-        //         // 새로운 tr 태그 생성 및 추가할 버튼 HTML 추가
-        //         const newTr = document.createElement('tr');
-        //         newTr.innerHTML = newButtonHtml;
-        //
-        //         // tr 태그를 추가할 위치에 추가
-        //         preview_row.parentNode.insertBefore(newTr, preview_row.nextSibling);
-        //     });
+            // 존재하지 않는 경우에만 추가
+            if (!categoryExists) {
+                const newButtonHtml = `
+                  <tr style="height: 65px; border-bottom: 2px solid #d5d2d2;">
+                    <td class="col-4" style="border-right: 2px solid #d5d2d2; font-family: NanumBarunGothic, serif;">
+                      <div class="text_detailCategory_preview">${categoryVal}</div>
+                    </td>
+                    <td class="col-4" style="border-right: 2px solid #d5d2d2; text-align: center; font-family: NanumBarunGothic, serif;">
+                      <button type="button" class="minus" onclick="fnCalcCnt('minus');">
+                        <div style="width: 100%; height: 100%; position: absolute; top: -20px;">-</div>
+                      </button>
+                      <input type="text" class="count_category" name="item_cnt" title="수량 입력" value="1" maxlength="1">
+                      <button type="button" class="plus" onclick="fnCalcCnt('plus');">
+                        <div style="width: 100%; height: 100%; position: absolute; top: -20px;">+</div>
+                      </button>
+                    </td>
+                    <td class="col-4">
+                      <div class="button_inputCategory_delete">
+                        <input type="button" onclick="categoryDel()" value="X" style="color: red; border: none; font-size: 28px;">
+                      </div>
+                    </td>
+                  </tr>
+                `;
+                $("#preview_row").append(newButtonHtml);
+            }
         }
+
     });
 });
 
 
-//gpt 답변. 텍스트 길이에 따라 버튼 사이즈 조절
+// 카테고리 수량 조절 이벤트
+function fnCalcCnt(action) {
+    const row = event.target.closest('tr'); // 클릭된 버튼이 속한 행 가져오기
+    const countInput = row.querySelector('.count_category'); // 행에서 count_category input 찾기
+    let count = parseInt(countInput.value); // 현재 count 값 가져오기
 
-//     var myButton = document.getElementById("myButton"); // 버튼 요소 가져오기
-//     var buttonWidth = myButton.offsetWidth; // 버튼의 초기 너비 저장
-//
-//     // 버튼의 텍스트 내용이 변경될 때마다 실행되는 함수
-//     function adjustButtonWidth() {
-//     var buttonValue = myButton.value; // 버튼의 텍스트 내용 가져오기
-//     var textWidth = getTextWidth(buttonValue); // 버튼의 텍스트 길이 측정하기
-//     var newWidth = Math.max(textWidth, buttonWidth); // 버튼의 너비 결정하기
-//
-//     myButton.style.width = newWidth + "px"; // 버튼의 너비 설정하기
-// }
-//
-//     // 텍스트 길이를 측정하는 함수
-//     function getTextWidth(text) {
-//     var span = document.createElement("span"); // 측정용 span 요소 생성
-//     span.style.visibility = "hidden"; // span 요소를 화면에서 숨김
-//     span.style.whiteSpace = "nowrap"; // span 요소를 한 줄로 고정
-//     span.innerHTML = text; // span 요소의 텍스트 내용 설정
-//
-//     document.body.appendChild(span); // span 요소를 body에 추가
-//
-//     var width = span.offsetWidth; // span 요소의 너비 측정
-//
-//     document.body.removeChild(span); // span 요소를 body에서 제거
-//
-//     return width;
-// }
-//
-//     // 버튼의 텍스트 내용이 변경될 때 adjustButtonWidth 함수를 실행하도록 이벤트 리스너 등록
-//     myButton.addEventListener("input", adjustButtonWidth);
+    if (action === 'plus') {
+        count++;
+    } else if (action === 'minus') {
+        if (count > 1) { // count 값이 1 이상일 때만 감소
+            count--;
+        }
+    }
+
+    countInput.value = count; // 변경된 count 값을 count_category input에 적용
+}
+
+// 카테고리 삭제 이벤트
+function categoryDel() {
+    $(event.target).closest('tr').remove();  // 현재 클릭된 삭제 버튼이 속한 행을 제거
+}
+
+// 다음 페이지 이동시, 수량 입력여부 확인
+function chBox() {
+    var previewRow = document.getElementById("preview_row");
+    var closestRowWithChildNodes = previewRow.closest('tbody:has(>*)');
+    if (closestRowWithChildNodes != null) {
+        location.href = "/pickup3";
+    } else {
+        alert("카테고리를 선택해주세요.");
+        return false;
+    }
+}
+
